3ef28f896e9b8e0712c9096796eb25fc7cb7f392
Dennis-Mwea <Mweadennis2@gmail.com>
2018-12-01 23:26:51 +0300

    Database factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,11 +14,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'username' => $faker->unique()->word.rand(1, 73),
+        'password' => $password ?: $password = bcrypt('secret'),
+        'avatar' => 'https://randomuser.me/api/portraits/'.$faker->randomElement(['men', 'women']).'/'.rand(1, 99).'.jpg',
+        'cover' => $faker->imageUrl(800, 220, 'nature'),
         'remember_token' => str_random(10),
     ];
 });
