54e9bbea6c3053c55c7d0fde418d1d3702176d53
Dennis-Mwea <Mweadennis2@gmail.com>
2018-12-11 11:01:39 +0300

    Tweet Type

---
+++ app/GraphQL/Type/TweetType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as GraphQLType;
+
+class TweetType extends GraphQLType
+{
+    public function fields()
+    {
+        return [
+            'id' => Type::nonNull(Type::int()),
+            'body' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'user_id' => [
+                'type' => Type::nonNull(Type::int()),
+            ],
+            'created_at' => [
+                'type' => Type::string(),
+            ],
+            'updated_at' => [
+                'type' => Type::string(),
+            ],
+            // Counts
+            'replies_count' => [
+                'type' => Type::int(),
+            ],
+            'likes_count' => [
+                'type' => Type::int(),
+            ],
+            // Nested Resource
+            'user' => [
+              'type' => GraphQL::type('User'),
+            ],
+            'replies' => [
+                'args' => [
+                    'id' => [
+                        'type' => Type::int(),
+                    ],
+                    'first' => [
+                        'type' => Type::int(),
+                    ],
+                ],
+                'type' => Type::listOf(GraphQL::type('Reply')),
+            ],
+            'likes' => [
+                'args' => [
+                    'id' => [
+                        'type' => Type::int(),
+                    ],
+                    'first' => [
+                        'type' => Type::int(),
+                    ],
+                ],
+                'type' => Type::listOf(GraphQL::type('Like')),
+            ],
+            ];
+    }
+
+    /**
+     * If you want to resolve the field yourself, you can declare a method
+     * with the following format resolve[FIELD_NAME]Field().
+     */
+    protected function resolveBodyField($root, $args)
+    {
+        return nl2br($root->body);
+    }
+
+    protected function resolveCreatedAtField($root, $args)
+    {
+        return (string) $root->created_at->diffForHumans();
+    }
+
+    protected function resolveUpdatedAtField($root, $args)
+    {
+        return (string) $root->updated_at->toDayDateTimeString();
+    }
+}
